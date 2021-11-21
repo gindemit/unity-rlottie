ffda3342ecad293dc368cd511f625ed067cff4f0
Gindemit Konstantin <k.hindemit@rambler.ru>
2021-11-21 16:32:19 +0100

    ~ Work in progress in AnimatedButton implementation

--- unity/RLottieUnity/Assets/LottiePlugin/src/UI/AnimatedButton.cs
+++ unity/RLottieUnity/Assets/LottiePlugin/src/UI/AnimatedButton.cs
@@ -1,9 +1,11 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace LottiePlugin.UI
 {
-    public sealed class AnimatedButton : Button
+    public sealed class AnimatedButton : Selectable, IPointerClickHandler, ISubmitHandler
     {
         [System.Serializable]
         public struct State
@@ -12,6 +14,9 @@ namespace LottiePlugin.UI
             public int FrameNumber;
             public bool StayHere;
         }
+        [System.Serializable]
+        public class ButtonClickedEvent : UnityEngine.Events.UnityEvent<int, State> { }
+
 
         internal TextAsset AnimationJson => _animationJson;
         internal uint TextureWidth => _textureWidth;
@@ -20,14 +25,69 @@ namespace LottiePlugin.UI
         internal Graphic Graphic => _graphic;
         internal State[] States => _states;
 
-        private State _currentState;
-
-
         [SerializeField] private TextAsset _animationJson;
         [SerializeField] private uint _textureWidth;
         [SerializeField] private uint _textureHeight;
         [SerializeField] private Graphic _graphic;
         [SerializeField] private State[] _states;
+        [SerializeField] private ButtonClickedEvent _onClick = new ButtonClickedEvent();
 
+        private int _currentStateIndex;
+        private LottieAnimation _lottieAnimation;
+
+        protected override void Start()
+        {
+            base.Start();
+            _lottieAnimation = LottieAnimation.LoadFromJsonData(
+                _animationJson.text,
+                string.Empty,
+                _textureWidth,
+                _textureHeight);
+            _lottieAnimation.DrawOneFrame(_states[0].FrameNumber);
+            ((RawImage)_graphic).texture = _lottieAnimation.Texture;
+        }
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            _lottieAnimation?.Dispose();
+            _lottieAnimation = null;
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (eventData.button != PointerEventData.InputButton.Left)
+                return;
+
+            Press();
+        }
+        public void OnSubmit(BaseEventData eventData)
+        {
+            Press();
+        }
+
+        private void Press()
+        {
+            if (!IsActive() || !IsInteractable())
+            {
+                return;
+            }
+            _onClick.Invoke(_currentStateIndex, _states[_currentStateIndex]);
+            StartCoroutine(AnimateToNextState());
+        }
+        private IEnumerator AnimateToNextState()
+        {
+            _currentStateIndex++;
+            if (_currentStateIndex >= _states.Length)
+            {
+                _currentStateIndex = 0;
+            }
+            _lottieAnimation.Play();
+            State nextState = _states[_currentStateIndex];
+            while (_lottieAnimation.CurrentFrame < nextState.FrameNumber)
+            {
+                _lottieAnimation.Update();
+                yield return null;
+            }
+        }
     }
 }
