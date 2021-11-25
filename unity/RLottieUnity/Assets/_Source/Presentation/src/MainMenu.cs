d3b70d846413841a8d708cb6b38be20d828a331e
Gindemit Konstantin <k.hindemit@rambler.ru>
2021-11-25 22:57:30 +0100

    ~ Added listeners to menu buttons

--- unity/RLottieUnity/Assets/_Source/Presentation/src/MainMenu.cs
+++ unity/RLottieUnity/Assets/_Source/Presentation/src/MainMenu.cs
@@ -1,18 +1,42 @@
-﻿using UnityEngine;
+using System;
+using LottiePlugin.UI;
+using UnityEngine;
 
 namespace Presentation
 {
     internal sealed class MainMenu : MonoBehaviour
     {
         [SerializeField] private LottiePlayerScreen _lottiePlayerScreen;
+        [SerializeField] private AnimatedButton _homeButton;
+        [SerializeField] private AnimatedButton _playerButton;
+        [SerializeField] private AnimatedButton _exploreButton;
 
         private void Awake()
         {
+            _homeButton.OnClick.AddListener(OnHomeButtonClick);
+            _playerButton.OnClick.AddListener(OnPlayerButtonClick);
+            _exploreButton.OnClick.AddListener(OnExploreButtonClick);
             _lottiePlayerScreen.Init();
         }
         private void OnDestroy()
         {
+            _homeButton.OnClick.RemoveListener(OnHomeButtonClick);
+            _playerButton.OnClick.RemoveListener(OnPlayerButtonClick);
+            _exploreButton.OnClick.RemoveListener(OnExploreButtonClick);
             _lottiePlayerScreen.Dispose();
+        }
+
+        private void OnHomeButtonClick(int stateIndex, AnimatedButton.State state)
+        {
+
+        }
+        private void OnPlayerButtonClick(int stateIndex, AnimatedButton.State state)
+        {
+
+        }
+        private void OnExploreButtonClick(int stateIndex, AnimatedButton.State state)
+        {
+
         }
     }
 }
