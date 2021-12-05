aa1735ae6003d5c932166d1576bfda9a71cabfd4
Gindemit Konstantin <k.hindemit@rambler.ru>
2021-12-05 18:23:40 +0100

    + Added ExploreTelegramStickers screen. Not yet implemented

---
+++ unity/RLottieUnity/Assets/_Source/Presentation/src/UI/ExploreTelegramStickers.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace Presentation.UI
+{
+    internal sealed class ExploreTelegramStickers : MonoBehaviour, System.IDisposable
+    {
+        [SerializeField] private AnimationPreview _animationPreviewPrefab;
+
+        internal void Init()
+        {
+
+        }
+        public void Dispose()
+        {
+
+        }
+    }
+}
