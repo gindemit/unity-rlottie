4541ec0478024a4f8c6f078970554de00817019a
Gindemit Konstantin <k.hindemit@rambler.ru>
2021-12-15 21:48:00 +0100

    ~ Added ExploreTelegramStickers screen to SampleScene.unity.

--- unity/RLottieUnity/Assets/_Source/Presentation/src/UI/ExploreTelegramStickers.cs
+++ unity/RLottieUnity/Assets/_Source/Presentation/src/UI/ExploreTelegramStickers.cs
@@ -4,7 +4,9 @@ namespace Presentation.UI
 {
     internal sealed class ExploreTelegramStickers : MonoBehaviour, System.IDisposable
     {
-        [SerializeField] private AnimationPreview _animationPreviewPrefab;
+        [SerializeField] private LottieAnimationsPreview _lottieAnimationsPreview;
+        [SerializeField] private TMPro.TMP_InputField _tokenInputField;
+        [SerializeField] private TMPro.TMP_InputField _stickerPackNameInputField;
 
         internal void Init()
         {
