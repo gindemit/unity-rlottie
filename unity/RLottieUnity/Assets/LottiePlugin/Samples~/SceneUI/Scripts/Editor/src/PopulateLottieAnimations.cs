5c2e5d96893f328b28f86a4e8091b34b7ec8cbb8
Konstantin Gindemit <konstantin.gindemit@gmail.com>
2023-05-06 15:16:32 +0200

    ~ The PopulateLottieAnimationsWindow is now an editor window. It is used to update the LottieAnimationsArray.asset

--- unity/RLottieUnity/Assets/LottiePlugin/Samples~/SceneUI/Scripts/Editor/src/PopulateLottieAnimations.cs
+++ unity/RLottieUnity/Assets/LottiePlugin/Samples~/SceneUI/Scripts/Editor/src/PopulateLottieAnimations.cs
@@ -1,51 +1,116 @@
+using LottiePlugin.Sample.SceneUI.Data;
 using System;
 using System.IO;
-using LottiePlugin.Sample.SceneUI.Data;
 using UnityEditor;
 using UnityEngine;
 
-namespace LottiePlugin.Sample.SceneUI.Editor.Menu
+namespace LottiePlugin.Sample.SceneUI.Editor
 {
-    internal static class PopulateLottieAnimations
+    public class PopulateLottieAnimationsWindow : EditorWindow
     {
+        private LottieAnimations lottieAnimations;
+        private SerializedObject serializedObject;
+        private SerializedProperty lottieAnimationsArray;
+        private string folderPath;
+
         [MenuItem("Tools/Populate Lottie Animations")]
-        internal static void Add()
+        public static void ShowWindow()
         {
-            UnityEngine.Object[] selectedObjects = Selection.objects;
-            if (selectedObjects.Length != 1)
+            GetWindow<PopulateLottieAnimationsWindow>("Populate Lottie Animations");
+        }
+
+        private void OnEnable()
+        {
+            if (lottieAnimations != null)
             {
-                Debug.LogWarning("Please select one folder to populate the animations array");
+                serializedObject = new SerializedObject(lottieAnimations);
+                lottieAnimationsArray = serializedObject.FindProperty("_animations");
+            }
+        }
+
+        private void OnGUI()
+        {
+            EditorGUILayout.LabelField("Select Lottie Animations Array", EditorStyles.boldLabel);
+            EditorGUI.BeginChangeCheck();
+            lottieAnimations = EditorGUILayout.ObjectField("Lottie Animations Array", lottieAnimations, typeof(LottieAnimations), false) as LottieAnimations;
+            if (EditorGUI.EndChangeCheck())
+            {
+                serializedObject = new SerializedObject(lottieAnimations);
+                lottieAnimationsArray = serializedObject.FindProperty("_animations");
+            }
+
+            EditorGUILayout.Space();
+
+            EditorGUILayout.LabelField("Select the folder inside StreamingAssets folder", EditorStyles.boldLabel);
+            folderPath = EditorGUILayout.TextField("Folder Path", folderPath);
+            if (GUILayout.Button("Select Folder"))
+            {
+                string currentProjectStreamingAssetsFolder = Application.dataPath + "/StreamingAssets";
+                string path = EditorUtility.OpenFolderPanel(
+                    "Select Lottie Animations Folder",
+                    currentProjectStreamingAssetsFolder,
+                    "StreamingAssets");
+                if (!string.IsNullOrEmpty(path) && path.StartsWith(currentProjectStreamingAssetsFolder))
+                {
+                    folderPath = "Assets" + path.Substring(Application.dataPath.Length);
+                }
+                else
+                {
+                    EditorUtility.DisplayDialog("Error", "Please select a folder inside of Streaming Assets current projects folder folder", "OK");
+                }
+            }
+
+            if (GUILayout.Button("Populate Lottie Animations"))
+            {
+                PopulateLottieAnimations();
+            }
+        }
+
+        private void PopulateLottieAnimations()
+        {
+            if (lottieAnimations == null)
+            {
+                EditorUtility.DisplayDialog("Error", "Please select a Lottie Animations Array asset", "OK");
                 return;
             }
-            UnityEngine.Object obj = selectedObjects[0];
-            string path = AssetDatabase.GetAssetPath(obj.GetInstanceID());
-            if (!Directory.Exists(path))
+
+            if (string.IsNullOrEmpty(folderPath))
             {
-                Debug.LogWarning("Please select a folder to populate the animations array");
+                EditorUtility.DisplayDialog("Error", "Please select a folder to populate the animations array", "OK");
                 return;
             }
-            if (!path.StartsWith("Assets/StreamingAssets"))
+            if (!Directory.Exists(folderPath))
             {
-                Debug.LogWarning("Please select a folder inside of Streaming Assets folder");
+                EditorUtility.DisplayDialog("Error", "Folder does not exist. Please select a valid folder.", "OK");
                 return;
             }
-            LottieAnimations lottieAnimations =
-                AssetDatabase.LoadAssetAtPath<LottieAnimations>("Assets/_ScriptableObjects/LottieAnimationsArray.asset");
-            string[] allFiles = Directory.GetFiles(path, "*.json", SearchOption.AllDirectories);
+
+            string[] allFiles = Directory.GetFiles(folderPath, "*.json", SearchOption.AllDirectories);
             string[] finalFilePaths = new string[allFiles.Length];
             Array.Sort(allFiles);
             for (int i = 0; i < allFiles.Length; ++i)
             {
-                ReadOnlySpan<char> filePath = allFiles[i].AsSpan();
+                string filePath = allFiles[i];
                 const int ASSETS_STREAMINGASSETS_FOLDER_PREFIX_LENGTH = 23;
                 const int DOT_JSON_EXTENTION_LENGTH = 5;
-                ReadOnlySpan<char> final = filePath.Slice(
+                string final = filePath.Substring(
                     ASSETS_STREAMINGASSETS_FOLDER_PREFIX_LENGTH,
                     filePath.Length - ASSETS_STREAMINGASSETS_FOLDER_PREFIX_LENGTH - DOT_JSON_EXTENTION_LENGTH);
-                finalFilePaths[i] = final.ToString().Replace("\\", "/");//TODO: find a better solution than "Replace"
+                finalFilePaths[i] = final.Replace("\\", "/"); //TODO: find a better solution than "Replace"
             }
-            lottieAnimations.SetAnimationsEditorCall(finalFilePaths);
+
+            lottieAnimationsArray.ClearArray();
+            for (int i = 0; i < finalFilePaths.Length; ++i)
+            {
+                lottieAnimationsArray.InsertArrayElementAtIndex(i);
+                lottieAnimationsArray.GetArrayElementAtIndex(i).stringValue = finalFilePaths[i];
+            }
+
+            serializedObject.ApplyModifiedProperties();
             EditorUtility.SetDirty(lottieAnimations);
+            AssetDatabase.SaveAssets();
+            EditorUtility.DisplayDialog("Success!", "Lottie animations asset updated", "OK");
+            Selection.activeObject = lottieAnimations;
         }
     }
 }
