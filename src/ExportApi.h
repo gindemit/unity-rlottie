3c22a98d51085a72d2d62d9ec0235a04eb5dbaf6
Konstantin Gindemit <konstantin.gindemit@gmail.com>
2023-07-03 22:33:50 +0200

    ~ Trying to fix export functions issues for Android.

--- src/ExportApi.h
+++ src/ExportApi.h
@@ -3,10 +3,8 @@
 
 #if _MSC_VER // this is defined when compiling with Visual Studio
 #define EXPORT_API __declspec(dllexport) // Visual Studio needs annotating exported functions with this
-#elif RLOTTIE_OSX
-#define EXPORT_API extern "C" __attribute__((visibility("default"))) // XCode on Macos needs export for cpp files
 #else
-#define EXPORT_API // Android Studio does not need annotating exported functions, so define is empty
+#define EXPORT_API __attribute__((visibility("default"))) // Other compilers
 #endif
 
 #endif // !_EXPORT_API_H_
