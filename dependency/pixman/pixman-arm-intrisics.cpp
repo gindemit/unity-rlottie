b72a6d09fd6855db1df805b0677f609281a2d375
Konstantin Gindemit <konstantin.gindemit@gmail.com>
2023-07-11 18:16:23 +0200

    ~ Updated the pixman_composite_over_n_8888_asm_neon function implementation.

--- dependency/pixman/pixman-arm-intrisics.cpp
+++ dependency/pixman/pixman-arm-intrisics.cpp
@@ -1,5 +1,9 @@
 #include <arm_neon.h>
 
+#define BYTE_MUL(c, a)                                  \
+    ((((((c) >> 8) & 0x00ff00ff) * (a)) & 0xff00ff00) + \
+     (((((c)&0x00ff00ff) * (a)) >> 8) & 0x00ff00ff))
+
 extern "C" void pixman_composite_src_n_8888_asm_neon(int32_t w, int32_t h,
                                                      uint32_t *dst,
                                                      int32_t   dst_stride,
@@ -27,24 +31,45 @@ extern "C" void pixman_composite_src_n_8888_asm_neon(int32_t w, int32_t h,
 extern "C" void pixman_composite_over_n_8888_asm_neon(int32_t w, int32_t h,
                                                       uint32_t *dst,
                                                       int32_t   dst_stride,
-                                                      uint32_t  src) {
-    // Since neon intrinsics handle 128 bits at a time, or 4 pixels of 32 bits each,
-    // we need to adjust the width accordingly.
-    int adjusted_w = w / 4;
-    for (int j = 0; j < h; ++j) {
-        for (int i = 0; i < adjusted_w; ++i) {
-            // Initialization. It seems to just duplicate the source value across vectors
-            uint32x4_t v0 = vdupq_n_u32(src);
-            uint32x4_t v1 = vdupq_n_u32(src);
-            uint32x4_t v2 = vdupq_n_u32(src);
-            uint32x4_t v3 = vdupq_n_u32(src);
+                                                      uint32_t  color) {
+    // Calculate alpha
+    int alpha = color >> 24;
+    int ialpha = 255 - alpha;
 
-            // Storing to destination
-            vst1q_u32(dst + i * 4, v0);
-            vst1q_u32(dst + i * 4 + 4, v1);
-            vst1q_u32(dst + i * 4 + 8, v2);
-            vst1q_u32(dst + i * 4 + 12, v3);
-        }
-        dst += dst_stride;
+    // Calculate total length
+    int total_len = w * h;
+
+    // create a 4 element vector with the source color
+    uint32x4_t v_color = vdupq_n_u32(color);
+    
+    // create a 4 element vector with the inverted alpha value
+    uint8x16_t v_ialpha = vdupq_n_u8(ialpha);
+    
+    // iterate over the array 4 elements at a time
+    for (int i = 0; i < total_len; i += 4)
+    {
+        // load 4 elements from the destination array
+        uint32x4_t v_dest = vld1q_u32(&dst[i]);
+        
+        // unpack the destination colors into 8-bit elements
+        uint8x16_t v_dest8 = vreinterpretq_u8_u32(v_dest);
+
+        // multiply the destination color by the inverted alpha
+        uint8x16_t v_dest_scaled = vmulq_u8(v_dest8, v_ialpha);
+        
+        // repack the result into 32-bit elements
+        uint32x4_t v_dest_scaled32 = vreinterpretq_u32_u8(v_dest_scaled);
+        
+        // add the source color to the result
+        uint32x4_t v_result = vaddq_u32(v_color, v_dest_scaled32);
+        
+        // store the result back into the destination array
+        vst1q_u32(&dst[i], v_result);
+    }
+
+    // If the total length is not a multiple of 4, we need to finish the rest
+    for (int i = total_len & ~3; i < total_len; ++i)
+    {
+        dst[i] = color + BYTE_MUL(dst[i], ialpha);
     }
 }
